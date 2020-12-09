025abf258a77f59773f53727a7fe42f17f314e1d
Petar Srzentic <petarsrzentic@gmail.com>
2020-12-09 17:41:00 +0100

    Splash screen

---
+++ app/src/main/java/com/petarsrzentic/insurance/ui/SplashScreen.kt
@@ -0,0 +1,39 @@
+package com.petarsrzentic.insurance.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.petarsrzentic.insurance.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+       val motion = findViewById<MotionLayout>(R.id.splash_screen)
+        motion.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+               startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+        })
+
+
+    }
+}
